86373d543346bb047444026fe3641bc6efab71a3
Thanh Tung <tungdore77@gmail.com>
2022-07-03 14:12:21 +0700

    up code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,9 @@
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
+                @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                @endif
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\AddTimesheetController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware([
